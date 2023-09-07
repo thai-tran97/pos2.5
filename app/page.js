335ca964bd558c2b8d6397694a06a67a992a4fef
thai-tran97 <thaitran.2000@yahoo.com>
2023-09-06 19:03:55 -0700

    added time to dashboard

--- app/page.js
+++ app/page.js
@@ -2,6 +2,9 @@ import Image from 'next/image'
 import ImageButton from '@/components/ImageButton'
 
 export default function Home() {
+
+  var date = new Date().toLocaleString();
+
   return (
     <div
       className="h-screen w-screen"
@@ -10,6 +13,11 @@ export default function Home() {
       <div
         className="h-full w-full flex align-center items-center justify-center"
       >
+        <div className='date-time-banner' 
+            style={{position:"absolute", top:"50px", backgroundColor:"black", color: "white", border:"solid 2px black"}}
+        >
+            <h1>{date}</h1>
+        </div>
         <div
           className="bg-gray-300 h-[28rem] w-[40rem] rounded-lg flex flex-col items-center"
         >
@@ -23,7 +31,7 @@ export default function Home() {
             <ImageButton image="cale.png" imageName="calendar" name="Calendar" link="calendar"/>
             <ImageButton image="clock.png" imageName="clock" name="Clock-in" link="clockin"/>
             <ImageButton image="em.png" imageName="employee" name="Employee" link="employee"/>
-            <ImageButton image="cas.png" imageName="admin" name="Admin" link="admin"/>
+            <ImageButton image="ad.png" imageName="admin" name="Admin" link="admin"/>
             <ImageButton image="se.png" imageName="setting" name="Setting" link="setting"/>
 
           </div>
